1c1de98d169ed2a8b73925c2ed7b053f31123840
超载 <chaozai@bicitech.cn>
2018-06-06 19:49:53 +0800

    项目结构修改

---
+++ showtime/src/main/java/indi/smt/uno/showtime/ShowtimeApplication.java
@@ -0,0 +1,12 @@
+package indi.smt.uno.showtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowtimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowtimeApplication.class, args);
+    }
+}
